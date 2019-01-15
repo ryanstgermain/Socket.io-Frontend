c3edc8cf7da4d6c2a519bb785ae5cf0f3fd94912
ryanstgermain <ryanjoel26@gmail.com>
2019-01-15 11:37:29 -0700

    added some more css and a few other things

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
 import Chat from './components/Chat.js'
 import './style/App.css'
+import socketIOClient from 'socket.io-client'
 
 class App extends Component {
+    constructor() {
+        super()
+
+    }
+
   render() {
     return (
       <div>
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,24 +2,34 @@ import React, { Component } from 'react'
 import { Button } from 'react-bootstrap'
 import '../style/App.css'
 
-const Chat = () => {
+class Chat extends Component {
+
+  render() {
     return (
         <div>
-            <h1>Socket.io Test App</h1>
+            <h1>Socket.io Chat Room</h1>
             <div className='main-container'>
                 <div className='content'>
                     <div className='message-list'>
-                        <div className='message'>
-                            <h6>Messages here</h6>
+                        <div className='message-block'>
+                            <div className='message-name hvr-backward'>
+                                <h6>Name:</h6>
+                            </div>
+                            <div className='message hvr-forward'>
+                                <h6>Messages here</h6>
+                            </div>
                         </div>
                     </div>
                     <div className='input-button'>
-                        <input autocomplete='off' /><Button className='button-style'>Send</Button>
+                        <input className='name' placeholder='Name' />
+                        <input placeholder='Message' autoComplete='off' />
+                        <Button className='button-style'>Send</Button>
                     </div>
                 </div>
             </div>
         </div>
     )
+  } 
 }
 
 export default Chat
